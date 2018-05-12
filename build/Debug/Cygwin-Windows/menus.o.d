36109128e064d538b2bedf09a1837933bec9706e
Edvin Maid <edvvin@mail.com>
2018-05-13 00:32:47 +0200

    Add files via upload

---
+++ build/Debug/Cygwin-Windows/menus.o.d
@@ -0,0 +1,6 @@
+build/Debug/Cygwin-Windows/menus.o: menus.c menus.h \
+ /cygdrive/c/Program\ Files/NetBeans\ 8.2/pdcurs36/curses.h
+
+menus.h:
+
+/cygdrive/c/Program\ Files/NetBeans\ 8.2/pdcurs36/curses.h:
