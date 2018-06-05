43fc7fefc41f8d63aa85b84d892665fd0c9ae7de
k0lic <kolicandrija98@gmail.com>
2018-06-06 01:10:32 +0200

    izneo Jovanovo djubre

--- hash1.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-long long mojHash(char *path,char fajlVecImaHash,char *kljuc,char duzinaKljuca,int metod);
-int Dodaj_ime_i_veliinu1(char *,char *,int);
-long long procitajHash(char *path);
-void upisiHash(char *path,long long hes);
