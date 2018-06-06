16cf5772cfd36de3154d6590acaaa1bea15c13fc
k0lic <kolicandrija98@gmail.com>
2018-06-06 18:26:24 +0200

    dokumentacija za doxygen

--- DES.c
+++ DES.c
@@ -21,23 +21,12 @@ char S[][64]={{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,0,15,7,4,14,2,13,1,10,6,12,
             {12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13},
             {4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12},
             {13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}};
-/*
-void ispisibinarni(char);
-void ispisiblok(blok);
-*/
-#define fixBugs 1337
-
-#ifndef BLOKJEDEFINISAN
-
-#define BLOKJEDEFINISAN 420
 
 typedef struct
 {
     unsigned char a[8];
 } blok;
 
-
-#endif
 
 blok nula()
 {
@@ -284,7 +273,7 @@ char* writePath(char* path,char* dest)//treba pozvati srand u mainu
 // nova verzije, iste brzine kao i stara?! nista nema smisla
 void DES_file(char* path,char** newpath,blok *keys,char sifraIfajlImaHash,char dozvola,char doRemovePadding,char *dest)     //sifraIfajlImaHash = 0 - enkripcija; 1 - dekripcija
 {
-    optimizeCode(fixBugs);
+    //optimizeCode(fixBugs);
     int i,j,poc,removePadding=8,startPosition=0,textLength;
     long int OGlength;
     FILE *f,*newf;
--- DES.h
+++ DES.h
@@ -1,24 +1,81 @@
 #pragma once
 
 
-#ifndef BLOKJEDEFINISAN
 
-#define BLOKJEDEFINISAN 420
+/**
+  * @file
+  * @author Andrija Kolic
+  * @brief Funkcije za enkripciju i dekripciju fajlova DES ili triple-DES algoritmom.
+  *
+  * Header fajl koji sadrzi sve funkcije koje korisnik moze da pozove koristeci interfejs ili konzolu.
+  * Sadrzi funkcije za enkripciju i dekripciju fajla koristeci DES algoritam, kao i funkcije za
+  * enkripciju i dekripciju fajla koristeci triple-DES algoritam.
+  *
+  */
 
-typedef struct
-{
-    unsigned char a[8];
-} blok;
+/**
+ * @brief Funkcija za enkripciju vise fajlova jednim kljucem
+ * @param[in] pKeyHead Lista kljuceva koje ce funkcija koristiti
+ * @param[in,out] pFileHead Fajlovi koje ce funkcija da enkriptuje
+ *
+ * Nakon izvrsenja funkcije, u strukturi Cvorf ce se nalaziti novo ime fajla kao
+ * i status enkripcije.
+ */
 
 
-#endif
-
-
-
+/**
+  * @brief Funkcija za enkripciju fajla koristeci DES algoritam
+  * @param[in] path Ime fajla koji se enkriptuje
+  * @param[in] c Kljuc koji se koristi za enkripciju, prilozen u vidu niza char-ova duzine 8
+  * @param[in] dest Destinacija enkriptovanog fajla, rezultujuci fajl ce biti kreiran na toj lokaciji
+  *
+  * Funkcija enkriptuje fajl sa imenom 'path' i ne menja dati string. Koristi kljuc koji ucitava iz stringa c,
+  * ne menjajuci njegov sadrzaj. Rezultujuci fajl ce se nalaziti na lokaciji 'dest' i imace ime originala, mozda
+  * neznatno izmenjeno. Funkcija vraca 0 ako je enkriptovanje uspesno izvrseno, a vraca -1 ako otvaranje fajla nije uspelo.
+  *
+  */
 int DES_encrypt_file(char* path,char *c,char *dest);   //c je duzine 8
+
+/**
+  * @brief Funkcija za dekripciju fajla koristeci DES algoritam
+  * @param[in] path Ime enkriptovanog fajla
+  * @param[in] c Kljuc koji se koristi za dekripciju, prilozen u vidu niza char-ova duzine 8
+  * @param[in] dest Destinacija dekriptovanog fajla, rezultujuci fajl ce biti kreiran na toj lokaciji
+  *
+  * Funkcija dekriptuje fajl sa imenom 'path' i ne menja dati string. Koristi kljuc koji ucitava iz stringa c,
+  * ne menjajuci njegov sadrzaj. Rezultujuci fajl ce se nalaziti na lokaciji 'dest' i imace ime originala, koje je
+  * upisano u enkriptovani fajl, mozda neznatno izmenjeno. Funkcija vraca 0 ako je enkriptovanje uspesno izvrseno,
+  * vraca -1 ako otvaranje fajla nije uspelo, a vraca 1 ako prilozeni kljuc nije odgovarajuci.
+  *
+  */
 int DES_decrypt_file(char* path,char *c,char *dest);   //c je duzine 8
 
-
+/**
+  * @brief Funkcija za enkripciju fajla koristeci tripe-DES algoritam
+  * @param[in] path Ime fajla koji se enkriptuje
+  * @param[in] c Kljuc koji se koristi za enkripciju, prilozen u vidu niza char-ova duzine 16
+  * @param[in] dest Destinacija enkriptovanog fajla, rezultujuci fajl ce biti kreiran na toj lokaciji
+  *
+  * Funkcija enkriptuje fajl sa imenom 'path' i ne menja dati string. Koristi kljuc koji ucitava iz stringa c,
+  * ne menjajuci njegov sadrzaj. Rezultujuci fajl ce se nalaziti na lokaciji 'dest' i imace ime originala, mozda
+  * neznatno izmenjeno. Funkcija vraca 0 ako je enkriptovanje uspesno izvrseno, a vraca -1 ako otvaranje fajla nije uspelo.
+  *
+  */
 int triple_DES_encrypt_file(char* path,char *c,char *dest);   //c je duzine 16
+
+/**
+  * @brief Funkcija za dekripciju fajla koristeci triple-DES algoritam
+  * @param[in] path Ime enkriptovanog fajla
+  * @param[in] c Kljuc koji se koristi za dekripciju, prilozen u vidu niza char-ova duzine 16
+  * @param[in] dest Destinacija dekriptovanog fajla, rezultujuci fajl ce biti kreiran na toj lokaciji
+  *
+  * Funkcija dekriptuje fajl sa imenom 'path' i ne menja dati string. Koristi kljuc koji ucitava iz stringa c,
+  * ne menjajuci njegov sadrzaj. Rezultujuci fajl ce se nalaziti na lokaciji 'dest' i imace ime originala, koje je
+  * upisano u enkriptovani fajl, mozda neznatno izmenjeno. Funkcija vraca 0 ako je enkriptovanje uspesno izvrseno,
+  * vraca -1 ako otvaranje fajla nije uspelo, a vraca 1 ako prilozeni kljuc nije odgovarajuci.
+  *
+  */
 int triple_DES_decrypt_file(char* path,char *c,char *dest);   //c je duzine 16
+
+
 
